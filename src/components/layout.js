b7ffa0b20b11428c893fd9f0a1e2cb2498771f59
Antony Fenton <antonyjfenton@gmail.com>
2018-08-13 23:41:26 +0100

    First commit

---
+++ src/components/layout.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+
+import './layout.css'
+
+const Layout = ({ children, data }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <link
+            href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700"
+            rel="stylesheet"
+          />>
+          <html lang="en" />
+        </Helmet>
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {children}
+        </div>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <div className="hero">
+      <h1>Craig & Lisa</h1>
+      <p className="text-center">13th July 2019</p>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
